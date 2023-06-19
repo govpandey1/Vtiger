9d685c8425efba917cee08aea8120b8a014d1a93
akashdeb <62165769+akashdeb@users.noreply.github.com>
2023-06-19 13:28:09 +0530

    Update GetDate.java

--- vtiger/src/test/java/practice/GetDate.java
+++ vtiger/src/test/java/practice/GetDate.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 import vtiger.genericUtility.JavaUtility;
 
-public class GetDate {
+public class GetSysDate {
 
 	public static void main(String[] args) {
 		
@@ -14,6 +14,9 @@ public class GetDate {
 //		System.out.println(date.getDayOfMonth());
 		
 		System.out.println(new JavaUtility().generateDateInYYYY_MM_DD_Format());
+		System.out.println("Change in Git");
+
+		
 	}
 
 }
