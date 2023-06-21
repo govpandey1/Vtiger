cd73b289b540cd735f83609ec07e85537a23adc4
akashdeb <akashdeb87@gmail.com>
2023-06-21 14:29:43 +0530

    Added maven compiler 1.8

--- vtiger/src/test/java/practice/GetDate.java
+++ vtiger/src/test/java/practice/GetDate.java
@@ -7,7 +7,6 @@ import vtiger.genericUtility.JavaUtility;
 public class GetDate {
 
 	public static void main(String[] args) {
-		
 //		LocalDateTime date = LocalDateTime.now();
 //		System.out.println(date.getYear());
 //		System.out.println(date.getMonthValue());
